ed44ef0e94d1f3b74b0c6e96dbf2bd3271a2d27f
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2026-02-21 14:52:59 +0100

    fix: move ggseg.formats from Depends to Imports
    Use @import ggseg.formats in roxygen instead of Depends. Core
    packages are on CRAN so Remotes are not needed.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' NSPN500 Cortical Atlas
 #'
 #' @return A [ggseg.formats::ggseg_atlas] object (cortical).
+#' @import ggseg.formats
 #' @export
 #' @family ggseg_atlases
 nspn500 <- function() .nspn500
